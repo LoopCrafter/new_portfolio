84ffba814b38ce0488dfdecff69b62d312af2788
Hamed <ostovari.eng@gmail.com>
2025-07-13 19:02:01 +0200

    Removed unused imports and improve the event listener functions

--- component/MourseTracker.tsx
+++ component/MourseTracker.tsx
@@ -1,23 +1,26 @@
-// components/Mouse.tsx
 "use client";
 import { gsap } from "gsap";
-import { useState } from "react";
 import { useGSAP } from "@gsap/react";
-import useMousePosition from "@/hooks/useMousePosition";
 
 const MouseTracker = () => {
   useGSAP(() => {
-    const cursor = document.querySelector(".cursor");
-    document.addEventListener("mousemove", (e) => {
-      gsap.to(cursor, {
-        x: e.clientX - 20,
-        y: e.clientY - 20,
-        duration: 0.7,
-        ease: "power4.out",
-        delay: 0.15,
-      });
-    });
+    document.addEventListener("mousemove", mouseTracker);
+
+    return () => {
+      document.removeEventListener("mousemove", mouseTracker);
+    };
   });
+
+  const mouseTracker = (e: any) => {
+    const cursor = document.querySelector(".cursor");
+    gsap.to(cursor, {
+      x: e.clientX - 20,
+      y: e.clientY - 20,
+      duration: 0.7,
+      ease: "power4.out",
+      delay: 0.15,
+    });
+  };
 
   return (
     <div className="cursor w-[10px] h-[10px] rounded-full fixed top-0 left-0 bg-amber-300 z-50"></div>
